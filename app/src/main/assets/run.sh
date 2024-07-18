ee1f2285e777876a4ceb4bdb07653926647ca11b
SnowWolf725 <snowwolf725@gmail.com>
2024-07-18 11:01:12 +0800

    Change ota path

--- app/src/main/assets/run.sh
+++ app/src/main/assets/run.sh
@@ -15,19 +15,19 @@ else
     chmod 777 /data/local/7za
 fi
 
-if [ -d "/data/local/update" ]; then
-    rm -rf /data/local/update
+if [ -d "/data/ota_package" ]; then
+    rm -rf /data/ota_package
 fi
 
 echo "开始解析ROM"
-/data/local/7za x "$zipDir"/"$rn" -r -o/data/local/update >/dev/null
+/data/local/7za x "$zipDir"/"$rn" -r -o/data/ota_package >/dev/null
 echo "解析完毕"
-chmod a+r -R /data/local/update
+chmod a+r -R /data/ota_package
 
-if [ -f "/data/local/update/payload.bin" ]; then
+if [ -f "/data/ota_package/payload.bin" ]; then
     echo "ROM核心文件校验成功"
-    source /data/local/update/payload_properties.txt
-    update_engine_client --payload=file:///data/local/update/payload.bin --update --headers="
+    source /data/ota_package/payload_properties.txt
+    update_engine_client --payload=file:///data/ota_package/payload.bin --update --headers="
 FILE_HASH=$FILE_HASH
 FILE_SIZE=$FILE_SIZE
 METADATA_HASH=$METADATA_HASH
